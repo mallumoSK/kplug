4fb74822392effeac926dca23d17d505c2a5280d
mallumo <develop.mallumo@gmail.com>
2020-11-19 16:12:01 +0100

    v0.1.8
     fix missing reference

--- src/android/stt/SttDataHolder.kt
+++ src/android/stt/SttDataHolder.kt
@@ -3,5 +3,6 @@ package tk.mallumo.cordova.kplug.stt
 data class SttDataHolder(
     var preferOffline: Boolean = false,
     var maxResults: Int = 10,
-    var autoContinue: Boolean = true
+    var autoContinue: Boolean = true,
+    var enableStartStopSound: Boolean = true
 )
